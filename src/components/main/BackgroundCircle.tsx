0c0c59ca9430b24272a95218ebe02ac10048506e
Damaroo <levkichigin@yandex.ru>
2025-02-10 16:19:40 +0300

    fix for deploy

--- src/components/main/BackgroundCircle.tsx
+++ src/components/main/BackgroundCircle.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef } from 'react'
+import { useContext } from 'react'
 import styled from 'styled-components'
 import { DataContext } from '../../hooks/DataContext'
 // Основной круг
